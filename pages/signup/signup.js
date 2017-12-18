afe539a06bcab569d753d8c7a955e2826f2bd25c
Chen Xiaoman <836932889@qq.com>
2017-12-18 12:18:25 +0800

    Initialize Bmob

--- pages/signup/signup.js
+++ pages/signup/signup.js
@@ -1,80 +1,67 @@
 var util = require('../../utils/util.js');
 
+// Initialize cloud server Bmob
+var Bmob = require('../../utils/bmob.js');
+
 //get app instance
 const app = getApp()
 
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
     date: "",
-    time: "10am-1pm",
+    time: "1pm-3pm",
     deadLine: "",
     isAgree: false,
     isSignedUp: false
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
+
     //Set formated Date
     var date = util.formatDate(new Date());
+
     //DeadLine is 2 days before eventDay
     var deadLine = util.formatDate(new Date(new Date().setDate(new Date().getDate() - 2)));
     this.setData({
       date: date,
       deadLine: deadLine
     })
+
+    var Diary = Bmob.Object.extend("diary");
+    var diary = new Diary();
+    diary.set("title", "hello");
+    diary.set("content", "hello world");
+    //添加数据，第一个入口参数是null
+    diary.save(null, {
+      success: function (result) {
+        // 添加成功，返回成功之后的objectId（注意：返回的属性名字是id，不是objectId），你还可以在Bmob的Web管理后台看到对应的数据
+        console.log("日记创建成功, objectId:" + result.id);
+      },
+      error: function (result, error) {
+        // 添加失败
+        console.log('创建日记失败');
+
+      }
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
     console.log("SignUp is Ready")
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function () {
 
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
 
   },
 
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage: function () {
 
   },
@@ -94,12 +81,14 @@ Page({
       this.hideNotice();
     }
   },
+
   //Show panel when tapped "Terms of Service"
   showNotice: function (e) {
     this.setData({
       'notice_status': true
     });
   },
+
   hideNotice: function (e) {
     this.setData({
       'notice_status': false
