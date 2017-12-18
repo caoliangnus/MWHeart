236554a3dd87fa23747d86113448c2adcd722128
caoliangnus <liangcao1213@yahoo.com>
2017-12-18 09:20:24 +0800

    SignUpTab: update signup.wxml

--- utils/util.js
+++ utils/util.js
@@ -14,6 +14,30 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
-module.exports = {
-  formatTime: formatTime
+const formatWeekDay = n => {
+  var weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+  
+  return weekday[n].toString();
 }
+
+const formatDate = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const weekDay = formatWeekDay(date.getDay())
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + weekDay
+}
+
+module.exports = {
+  formatTime: formatTime,
+  formatDate: formatDate
+}
+
