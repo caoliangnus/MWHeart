b61b953301a337d43194b3f527d39dc347cc4395
Chen Xiaoman <836932889@qq.com>
2017-12-19 21:55:35 +0800

    Change cloud server user table from "_User" to "user"

--- app.js
+++ app.js
@@ -17,42 +17,24 @@ App({
             var openid = user.get("authData").weapp.openid;
             console.log(user, 'user', user.id, res);
 
-            if (user.get("nickName")) {
-
-              // 第二次访问
-              console.log(user.get("nickName"), 'res.get("nickName")');
-
-              wx.setStorageSync('openid', openid)
-            } else {
-
-              //保存用户其他信息
+            // Store user info to cloud
               wx.getUserInfo({
                 success: function (result) {
-
-                  var userInfo = result.userInfo;
-                  var nickName = userInfo.nickName;
-                  var avatarUrl = userInfo.avatarUrl;
-
-                  var u = Bmob.Object.extend("user");
-                  var query = new Bmob.Query(u);
-                  // 这个 id 是要修改条目的 id，你在生成这个存储并成功时可以获取到，请看前面的文档
-                  query.get(user.id, {
-                    success: function (result) {
-                      // 自动绑定之前的账号
-
-                      result.set('nickName', nickName);
-                      result.set("userPic", avatarUrl);
-                      result.set("openid", openid);
-                      result.save();
-
-                    }
-                  });
+                  var U = Bmob.Object.extend("user");
+                  var u = new U();
+                  u.save({
+                    openid: openid
+                  }, {
+                      success: function (result) {
+                        console.log("saved openid to cloud")
+                      },
+                      error: function (result, error) {
+                        console.log("failed to save (user id alr exited)", error)
+                      }
+                    });
 
                 }
               });
-
-
-            }
 
           }, function (err) {
             console.log(err, 'errr');
@@ -94,7 +76,7 @@ App({
   getUserRealNameAndPhone: function () {
     var that = this
     // Get user real name and phone number
-    var u = Bmob.Object.extend("_User");
+    var u = Bmob.Object.extend("user");
     var query = new Bmob.Query(u);
     query.first({
       success: function (object) {
