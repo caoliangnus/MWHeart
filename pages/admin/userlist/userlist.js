690d2ff1ad53f932cbd8215960bc423718d91288
caoliangnus <liangcao1213@yahoo.com>
2017-12-22 12:29:55 +0800

    UserList: edit user

--- pages/admin/userlist/userlist.js
+++ pages/admin/userlist/userlist.js
@@ -12,7 +12,7 @@ Page({
    */
   data: {
     userInfo: {},
-    eventDate:"",
+    eventDate: "",
     isLoading: false,
     isModifyUser: false
   },
@@ -30,19 +30,19 @@ Page({
     getList(this);
 
     //Update Data
-    var isAllUserList = options.isAllUserList=="true"?true:false;
-    var isVolunteerList = options.isVolunteerList=="true" ? true : false;
-    var isWaitingList = options.isWaitingList=="true" ? true : false;
+    var isAllUserList = options.isAllUserList == "true" ? true : false;
+    var isVolunteerList = options.isVolunteerList == "true" ? true : false;
+    var isWaitingList = options.isWaitingList == "true" ? true : false;
     var eventDate = "";
-    if(isVolunteerList || isWaitingList){
+    if (isVolunteerList || isWaitingList) {
       eventDate = options.date;
     }
-    
+
     this.setData({
       isAllUserList: isAllUserList,
       isVolunteerList: isVolunteerList,
       isWaitingList: isWaitingList,
-      eventDate : eventDate,
+      eventDate: eventDate,
     })
   },
 
@@ -50,7 +50,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
@@ -65,35 +65,35 @@ Page({
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   },
 
   noneWindows: function () {
@@ -101,7 +101,8 @@ Page({
       isModifyUser: ""
     })
   },
-  
+
+  //Delete User After clicked Delete Button
   deleteUser: function (event) {
     var id = event.target.dataset.id;
     wx.showModal({
@@ -129,23 +130,24 @@ Page({
   },
 
 
+  //Open form after clicked Edit button
   toModifyUser: function (event) {
     var nowId = event.target.dataset.id;
     var nowName = event.target.dataset.name;
-    var nowStatus = event.target.dataset.status;
+    var nowPhone = event.target.dataset.phone;
     that.setData({
       isModifyUser: true,
       nowId: nowId,
       nowName: nowName,
-      nowStatus: nowStatus
+      nowPhone: nowPhone
     })
   },
+
+  //Form for modifying user
   modifyUser: function (e) {
     var t = this;
     modify(t, e)
   },
-
-
 })
 
 
@@ -173,39 +175,38 @@ function getList(t, k) {
 }
 
 
-
+//Upload data to BMob after clicked submit button on ModifyForm
 function modify(t, e) {
   var that = t;
   //Edit User
-  var modyId = e.detail.value.id;
   var modyName = e.detail.value.name;
-  var modyStatus = e.detail.value.status;
+  var modyPhone = e.detail.value.phone;
   var thatName = that.data.nowName;
-  var thatStatus = that.data.nowStatus;
-  if ((modyName != thatName || modyStatus != thatStatus)) {
-    if (modyName == "" || modyStatus == "") {
-      common.showTip('Name or Status can not be empty', 'isLoading');
+  var thatPhone = that.data.nowPhone;
+  console.log(thatName);
+  console.log(thatPhone);
+  if ((modyName != thatName || modyPhone != thatPhone)) {
+    if (modyName == "" || modyPhone == "") {
+      common.showTip('Name or Phone can not be empty', 'isLoading');
     }
     else {
-      console.log(modyName, modyStatus)
+      console.log(modyName, modyPhone)
       var Diary = Bmob.Object.extend("user");
       var query = new Bmob.Query(Diary);
       // 这个 id 是要修改条目的 id，你在生成这个存储并成功时可以获取到，请看前面的文档
       query.get(that.data.nowId, {
         success: function (result) {
-
+          console.log(result);
           // 回调中可以取得这个 GameScore 对象的一个实例，然后就可以修改它了
-          result.set('name', modyName);
-          result.set('status', modyStatus);
+          result.set('realName', modyName);
+          result.set('phone', modyPhone);
           result.save();
           common.showTip('User Updated', 'success', function () {
             that.onShow();
             that.setData({
-              modifyDiarys: false
+              isModifyUser: false
             })
           });
-
-          // The object was retrieved successfully.
         },
         error: function (object, error) {
 
@@ -213,12 +214,12 @@ function modify(t, e) {
       });
     }
   }
-  else if (modyTitle == "" || modyContent == "") {
+  else if (modyName == "" || modyPhone == "") {
     common.showTip('标题或内容不能为空', 'isLoading');
   }
   else {
     that.setData({
-      modifyDiarys: false
+      isModifyUser: false
     })
     common.showTip('修改成功', 'isLoading');
   }
