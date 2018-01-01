cd38e2d5ba0b2bae040269e8f62bc072b5437d50
Chen Xiaoman <836932889@qq.com>
2018-01-01 15:30:00 +0800

    Seperate about CIP and about CIP form

---
+++ pages/cipForm/cipForm.js
@@ -0,0 +1,13 @@
+
+Page({
+    data: {
+      url1: "You can get the CIP form from the following URL:",
+      url2:"https://pan.baidu.com/s/1gfMYY7x",
+      organiser1 :"What should I write under the column \"Name of organiser\" ?",
+      organiser2: "NVAC MWH.",
+      collection1: "By the end of each semester, please hand in your CIP form to project director in time and get the signature and stamp of endorser.",
+      collection2: "Project director will inform you the collection of CIP form.",
+      sm21: "What's the name of scholarship of SM2 student?",
+      sm22: "Science & Technology (S&T) Undergraduate Scholarship."
+    },
+})
