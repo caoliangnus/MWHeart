b79a8075755b2259d8f2d9308db1ce041889a2cd
Chen Xiaoman <836932889@qq.com>
2018-01-01 15:18:18 +0800

    Complete CIP page

---
+++ pages/cip/cip.js
@@ -0,0 +1,21 @@
+
+Page({
+    data: {
+      url1: "You can get the CIP form from the following URL",
+      url2:"https://pan.baidu.com/s/1gfMYY7x",
+      organiser1 :"What should I write under the column \"Name of organiser\" ?",
+      organiser2: "NVAC MWH",
+      collection1: "By the end of each semester, please hand in your CIP form to project director in time and get the signature and stamp of endorser.",
+      collection2: "Project director will inform you the collection of CIP form.",
+      sm21: "What's the name of scholarship of SM2 student?",
+      sm22: "Science & Technology (S&T) Undergraduate Scholarship",
+      auth1: "Who's the authority in charge of community involvement programmes in NUS?",
+      auth2: "Scholars Engagement Team From Office of Student Affairs",
+      auth3: "Contact email: <scholarsengage@nus.edu.sg>",
+      hour1: "What's the requirement of CIP hour?",
+      hour2: "80 hours before graduation",
+      hour3: "60 hours before 5th semester",
+      more1: "You can get a copy of official slides(version 2017) on CIP by Office of Student Affairs from the following URL",
+      more2: "https://pan.baidu.com/s/1hscVCKc"
+    },
+})
